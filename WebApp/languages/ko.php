acafda77405532f80e55ea070f0bb2eee5395124
DevAny <devany.kr@gmail.com>
2026-02-06 13:13:00 +0900

    add Korean Language

---
+++ WebApp/languages/ko.php
@@ -0,0 +1,104 @@
+<?php 
+//KOREAN
+
+$lang["page.new-transaction"] = "새 거래";
+$lang["page.transaction.added"] = "새 거래";
+$lang["page.transaction.duplicated"] = "기존 거래";
+$lang["page.transaction.updated"] = "기존 거래";
+$lang["page.show-transactions"] = "거래 내역 표시";
+$lang["page.settings"] = "설정 편집";
+$lang["page.settings.new"] = "새 설정";
+$lang["page.guide"] = "가이드";
+$lang["page.about"] = "정보";
+
+$lang["sec.username"] = "사용자 이름";
+$lang["sec.username.placeholder"] = "사용자 이름을 입력하세요";
+$lang["sec.password"] = "비밀번호";
+$lang["sec.password.placeholder"] = "비밀번호를 입력하세요";
+$lang["sec.login"] = "로그인";
+$lang["sec.logout"] = "로그아웃";
+
+$lang["return_to_menu"] = "메뉴로 돌아가기";
+
+$lang["db_successfully_updated_to_version"] = "데이터베이스가 다음 버전으로 성공적으로 업데이트되었습니다 ";
+
+$lang["trans.msg.add-next"] = "다음 추가";
+$lang["trans.msg.action-added.successfully"] = "성공적으로 추가되었습니다";
+$lang["trans.msg.action-duplicated.successfully"] = "성공적으로 복제되었습니다";
+$lang["trans.msg.action-updated.successfully"] = "성공적으로 업데이트되었습니다";
+
+$lang["trans.new.header"] = "새 거래 생성 중";
+$lang["trans.new.submit"] = "거래 생성";
+$lang["trans.update.header"] = "거래 편집 중";
+$lang["trans.update.submit"] = "거래 업데이트";
+$lang["trans.duplicate.header"] = "거래 복제 중";
+$lang["trans.duplicate.submit"] = "복제본 생성";
+$lang["trans.upload.label"] = "사진 촬영 또는 첨부 파일 업로드";
+
+$lang["trans.date"] = "날짜";
+$lang["trans.status"] = "상태";
+$lang["trans.status.none"] = "없음";
+$lang["trans.status.reconciled"] = "조정됨";
+$lang["trans.status.void"] = "무효";
+$lang["trans.status.follow-up"] = "후속 조치";
+$lang["trans.status.duplicate"] = "중복";
+$lang["trans.type"] = "유형";
+$lang["trans.type.withdrawal"] = "출금";
+$lang["trans.type.deposit"] = "입금";
+$lang["trans.type.transfer"] = "이체";
+$lang["trans.account"] = "계좌";
+$lang["trans.to-account"] = "받는 계좌";
+$lang["trans.payee"] = "거래처";
+$lang["trans.payee.placeholder"] = "거래처 선택";
+$lang["trans.category"] = "카테고리";
+$lang["trans.category.placeholder"] = "카테고리 선택";
+$lang["trans.sub-category"] = "하위 카테고리";
+$lang["trans.sub-category.placeholder"] = "하위 카테고리 선택";
+$lang["trans.amount"] = "금액";
+$lang["trans.amount.placeholder"] = "새 거래 금액";
+$lang["trans.notes"] = "메모";
+$lang["trans.notes.placeholder"] = "새 거래 메모";
+
+$lang["settings.new"] = "Money Manager EX 사용을 위해 새로운 설정을 입력하세요";
+$lang["settings.default-account"] = "기본 계좌";
+$lang["settings.language"] = "언어";
+$lang["settings.save"] = "설정 저장";
+$lang["settings.apply"] = "설정 적용";
+
+$lang["settings.authentication.section"] = "인증";
+$lang["settings.authentication.disable"] = "인증 비활성화 (권장하지 않음)";
+$lang["settings.authentication.username.placeholder"] = "새 사용자 이름 입력";
+$lang["settings.authentication.password.update.placeholder"] = "변경하려면 새 비밀번호 입력";
+$lang["settings.authentication.password.new.placeholder"] = "비밀번호 입력";
+$lang["settings.authentication.password.confirm.placeholder"] = "새 비밀번호 확인";
+
+$lang["settings.new-trans.section"] = "새 거래";
+$lang["settings.new-trans.disable-payee.placeholder"] = "거래처 관리 비활성화";
+$lang["settings.new-trans.disable-category.placeholder"] = "카테고리 관리 비활성화";
+
+$lang["settings.new-trans.section"] = "데스크톱 통합";
+
+$lang["guide.step1"] = "아래 데이터를 복사하여 데스크톱 애플리케이션에 붙여넣으세요.";
+$lang["guide.step2"] = "데스크톱에서 웹앱으로 은행 계좌 등을 동기화하려면 데스크톱 애플리케이션을 다시 여세요.";
+$lang["guide.step3"] = "웹앱 버전 사용을 시작하세요.";
+$lang["guide.webapp-url"] = "웹앱 URL";
+$lang["guide.desktop-guid"] = "데스크톱 GUID";
+
+$lang["show.no_pending_trans"] = "대기 중인 거래 없음";
+$lang["show.current_pending_trans"] = "현재 대기 중인 거래";
+$lang["show.delete_all_selected"] = "선택 항목 모두 삭제";
+$lang["show.add_new"] = "새로 추가";
+$lang["show.delete"] = "삭제";
+$lang["show.new"] = "새 거래";
+$lang["show.no_trans_selected"] = "선택된 거래가 없습니다!";
+
+/*
+ * THESE TRANSLATIONS ARE STILL NOT USED.
+ * THEY ARE READY FOR JAVASCRIPT FILES: functions-1.2.0.js and new_transaction-1.0.4.js
+ */
+$lang["browser_not_support_html5"] = "브라우저가 HTML5를 완전히 지원하지 않는 것 같습니다.\n\n필드 유효성 검사가 작동하지 않으니 주의하세요!";
+$lang["sec.password_not_matching"] = "비밀번호가 일치하지 않습니다!";
+$lang["attachment.upload_failed"] = "파일 업로드 시도 중 오류가 발생했습니다.";
+$lang["attachment.upload_canceled"] = "사용자가 업로드를 취소했거나 브라우저 연결이 끊겼습니다.";
+$lang["trans.msg.not_all_fields_are_valid"] = "모든 필드가 유효하지는 않습니다";
+$lang["trans.msg.do_you_want_to_add_new_payee"] = "새 거래처를 추가하시겠습니까";
